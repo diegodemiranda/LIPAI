2b0df1c830ed9a424646f5b2f07f89e11c2cd562
Diego de Miranda <diego.demiranda@icloud.com>
2025-12-12 17:22:55 -0300

    Delete src/semana_2/A4_funcoes_em_python directory

--- src/semana_2/A4_funcoes_em_python/exercicio_1.py
+++
@@ -1,7 +0,0 @@
-def retornar_soma(n1: float, n2: float, n3: float) -> float:
-    """Recebe três números e retorna a soma."""
-    return n1 + n2 + n3
-
-if __name__ == "__main__":
-    res = retornar_soma(10, 20, 30)
-    print(f"Resultado retornado: {res}")
--- src/semana_2/A4_funcoes_em_python/exercicio_3.py
+++
@@ -1,7 +0,0 @@
-def somar_args(*args: float) -> float:
-    """Recebe n argumentos numéricos e retorna a soma."""
-    return sum(args)
-
-if __name__ == "__main__":
-    # Podemos passar quantos argumentos quisermos
-    print(f"Soma args: {somar_args(1, 2, 3, 4, 5)}")
