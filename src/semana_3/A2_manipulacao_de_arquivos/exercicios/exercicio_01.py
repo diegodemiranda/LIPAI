3359086cb7fc51e93a4a925d083ce75eccd80a9e
Diego de Miranda <diego.demiranda@icloud.com>
2025-12-30 14:17:14 -0300

    Refactor function to load project data from file

--- src/semana_3/A2_manipulacao_de_arquivos/exercicios/exercicio_01.py
+++ src/semana_3/A2_manipulacao_de_arquivos/exercicios/exercicio_01.py
@@ -1,44 +1,23 @@
 """ Ex 02 - Carregar dados de projeto """
-def carregar
-dados
-_
-_projetos(nome
-_
-arquivo):
-"""Retorna uma tupla de dicionários com dados de projetos.
-"""
-with open(nome
-_
-arquivo,
-'r'
-, encoding='utf-8') as arq:
-projeto = []
-for linha in arq:
-codigo, titulo, responsavel = linha.strip().replace('\''
-'').split('
-,
-')
-projeto.append(
-{
-'codigo': codigo,
-'titulo': titulo,
-'responsavel': responsavel
-,
-}
-)
-return tuple(projeto)
-ARQUIVO = 'arquivo
-_
-ex02.txt'
-lista
-_
-dados = carregar
-dados
-_
-_projetos(ARQUIVO)
-print(type(lista
-_
-dados))
-print(lista
-_
-dados)
+def carregar_dados_projetos(nome_arquivo):
+    """Retorna uma tupla de dicionários com dados de projetos."""
+    with open(nome_arquivo, 'r', encoding='utf-8') as arq:
+       projeto = []
+       for linha in arq:
+           codigo, titulo, responsavel = linha.strip().replace('\''
+'').split(',') projeto.append(
+                   {
+                     'codigo': codigo,
+                     'titulo': titulo,
+                     'responsavel': responsavel,
+                   }
+           )
+
+       return tuple(projeto)
+
+ARQUIVO = 'arquivo_ex02.txt'
+
+lista_dados = carregar_dados_projetos(ARQUIVO)
+
+print(type(lista_dados))
+print(lista_dados)
