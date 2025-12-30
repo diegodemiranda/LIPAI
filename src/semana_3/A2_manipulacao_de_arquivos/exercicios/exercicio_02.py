7695162f82cda0301b7a196e8e84d1eb8f073809
Diego de Miranda <diego.demiranda@icloud.com>
2025-12-30 14:18:45 -0300

    Add function to load project data from a file
    The function 'carregar_dados_projetos' reads project data from a file and returns a tuple of dictionaries containing project details.

--- src/semana_3/A2_manipulacao_de_arquivos/exercicios/exercicio_02.py
+++ src/semana_3/A2_manipulacao_de_arquivos/exercicios/exercicio_02.py
@@ -1,81 +1,23 @@
-""" Ex 03 - Convertendo uma linha em dicionário genérico """
-def linha
-_para
-_
-dict(frase, lista
-_
-chaves):
-"""Recebe uma linha e uma lista de chaves e retorna um dicionário.
-"""
-# cria uma lista com separador na vírgula para cada elemento da lista
-lista
-_
-linha = frase.strip().replace("'"
-,
-"").split('
-,
-')
-if len(lista
-_
-linha) != len(lista
-_
-chaves):
-return f"{frase} e {lista
-_
-chaves} devem ter a mesma quantidade de itens"
-else:
-dicionario = {}
-for i in range(len(lista
-_
-chaves)):
-key = lista
-_
-chaves[i]
-valor = lista
-_
-linha[i]
-dicionario[key] = valor
-return dicionario
-# casos testes
-# caso teste 1
-LINHA = "SP000001,Maria da Silva,maria@email.com"
-chaves = ['prontuario'
-'nome'
-,
-,
-'email']
-print(linha
-_para
-_
-dict(LINHA, chaves))
-# caso teste 2
-LINHA2 = "banana,3"
-chaves2 = ['item'
-,
-'quantidade']
-print(linha
-_para
-_
-dict(LINHA2, chaves2))
-# caso teste 3
-TEXTO = 'pentium IV, 8gb, 120gb, False'
-itens = ['cpu'
-'memoria'
-'hd'
-,
-,
-,
-print(linha
-_para
-_
-dict(TEXTO, itens))
-'cd-room']
-# caso teste 4
-DATA = 'FM 104.5, canal 8, faixa cidadão'
-dados = ['radio'
-,
-'tv']
-print(linha
-_para
-_
-dict(DATA, dados))
+""" Ex 02 - Carregar dados de projeto """
+def carregar_dados_projetos(nome_arquivo):
+    """Retorna uma tupla de dicionários com dados de projetos."""
+    with open(nome_arquivo, 'r', encoding='utf-8') as arq:
+       projeto = []
+       for linha in arq:
+           codigo, titulo, responsavel = linha.strip().replace('\''
+'').split(',') projeto.append(
+                   {
+                     'codigo': codigo,
+                     'titulo': titulo,
+                     'responsavel': responsavel,
+                   }
+           )
+
+       return tuple(projeto)
+
+ARQUIVO = 'arquivo_ex02.txt'
+
+lista_dados = carregar_dados_projetos(ARQUIVO)
+
+print(type(lista_dados))
+print(lista_dados)
